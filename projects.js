782197090a8d4ad3cd79ac39556668b2c713c8c3
David Lau <d24lau@uwaterloo.ca>
2017-04-23 13:47:39 -0400

    dlau

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ if (!window.indexedDB) {
     window.alert("Your browser doesn't support a stable version of IndexedDB.");
 }
 
-
+var db;
 var request = window.indexedDB.open("BashboredTasksDB", 1);
 
 request.onerror = function(event) {
@@ -10,4 +10,5 @@ request.onerror = function(event) {
 };
 request.onsuccess = function(event) {
   window.alert("Database opened successfully.");
+  db = event.target.result;
 };
